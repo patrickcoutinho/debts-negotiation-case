0f3ba1cf622fa62a22dba81e54c9590dc4515e0a
Patrick Coutinho <patrick@istyle.com.br>
2022-07-18 22:30:26 -0300

    chore: added eslint and prettier

--- frontend/offer/webpack.config.js
+++ frontend/offer/webpack.config.js
@@ -1,34 +1,35 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index",
-  target: "web",
-  mode: "development",
-  devtool: "source-map",
+  entry: './src/index',
+  target: 'web',
+  mode: 'development',
+  devtool: 'source-map',
   resolve: {
-    extensions: [".jsx", ".js", ".tsx", ".ts", ".json"],
+    extensions: ['.jsx', '.js', '.tsx', '.ts', '.json'],
   },
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.js$/,
-        loader: "source-map-loader",
+        loader: 'source-map-loader',
       },
       {
         test: /\.(ts|tsx)$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
   ],
 };
