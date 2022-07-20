2b26ce8c47e27a88ae5422c4e19f49960dbaa3b8
Patrick Coutinho <patrick@istyle.com.br>
2022-07-20 11:58:30 -0300

    chore: added jest-axe

--- frontend/design-system/src/components/navbar/navbar.spec.tsx
+++ frontend/design-system/src/components/navbar/navbar.spec.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './navbar';
+import { axe } from 'jest-axe';
 import { render } from '@testing-library/react';
 
 describe('Navbar', () => {
-  test('should render Navbar correctly', () => {
+  test('should render Navbar correctly', async () => {
     const { container } = render(<Navbar />);
 
     expect(container).toMatchSnapshot();
+    expect(await axe(container)).toHaveNoViolations();
   });
 });
