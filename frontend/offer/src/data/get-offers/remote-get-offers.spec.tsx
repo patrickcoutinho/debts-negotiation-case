e57c7d3f4dee8f392d977c7900d424cba42dbce3
Patrick Coutinho <patrick@istyle.com.br>
2022-07-20 20:20:27 -0300

    test: created get-offers implementation

---
+++ frontend/offer/src/data/get-offers/remote-get-offers.spec.tsx
@@ -0,0 +1,53 @@
+import { DebtType, OfferModel } from '../../domain/models/offer';
+import { FetchHttpClientMock, HttpClient, HttpStatusCode } from '@shared/infra';
+import { GetOffers } from '../../domain/usecases/get-offers';
+import { RemoteGetOffers } from './remote-get-offers';
+
+type SubjectTypes = {
+  responseStatus?: HttpStatusCode;
+  responseBody?: any;
+};
+
+type SubjectResponse = {
+  subject: RemoteGetOffers;
+};
+
+const makeSubject = ({
+  responseStatus = HttpStatusCode.ok,
+  responseBody = null,
+}: SubjectTypes): SubjectResponse => {
+  const httpClientMock = new FetchHttpClientMock();
+  jest.spyOn(httpClientMock, 'request');
+
+  httpClientMock.responseStatus = responseStatus;
+  httpClientMock.responseBody = responseBody;
+
+  const subject = new RemoteGetOffers(httpClientMock);
+
+  return { subject };
+};
+
+describe('RemoteGetOffers', () => {
+  test('should get offers', async () => {
+    const mockResponse: GetOffers.Model[] = [
+      {
+        value: {
+          actual: 123.0,
+          original: 123.0,
+        },
+        date: new Date(),
+        contact: '12345677890',
+        description: '',
+        installmentPlans: [],
+        type: DebtType.creditCard,
+      },
+    ];
+
+    const { subject } = makeSubject({ responseBody: mockResponse });
+
+    const response = await subject.get();
+
+    expect(response.length).toBe(mockResponse.length);
+    expect(response).toEqual(mockResponse);
+  });
+});
