fffcfd08a079dc86b702530faad6d018554821eb
Patrick Coutinho <patrick@istyle.com.br>
2022-07-22 15:12:17 -0300

    feat: updated logo

--- frontend/design-system/src/components/logo/logo.tsx
+++ frontend/design-system/src/components/logo/logo.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import Logotype from '../../assets/logoipsum-logo.svg';
-import { Image } from '@chakra-ui/react';
+import Logotype from '../../assets/logo.svg';
+import LogotypeDark from '../../assets/logo-dark.svg';
+import { Image, useColorModeValue } from '@chakra-ui/react';
 
 export const Logo = (props: any) => {
-  return <Image src={Logotype} {...props} />;
+  return <Image src={useColorModeValue(Logotype, LogotypeDark)} {...props} />;
 };
