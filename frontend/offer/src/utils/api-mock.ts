3f161d5ef7bb6f2b0e1ea0cb5b76a5c976328cb9
Patrick Coutinho <patrick@istyle.com.br>
2022-07-27 22:41:04 -0300

    chore: updated mock

--- frontend/offer/src/utils/api-mock.ts
+++ frontend/offer/src/utils/api-mock.ts
@@ -1,59 +1,40 @@
 import { DebtType } from '../domain/models/offer';
 import { GetOffers } from '../domain/usecases/get-offers';
+import { faker } from '@faker-js/faker';
 
+const item = {
+  id: faker.datatype.uuid(),
+  type: DebtType.creditCard,
+  contract: '12345677890',
+  date: new Date(),
+  value: {
+    actual: 9000.0,
+    original: 3000.0,
+  },
+  description:
+    'Lorem ipsum sit gravida torquent ipsum sapien nibh ante ornare fusce, aliquam habitant pellentesque magna turpis viverra blandit ad leo. ',
+  installmentPlans: [
+    {
+      installments: 1,
+      installmentValue: 870.97,
+      discount: 90,
+      total: 870.97,
+    },
+    {
+      installments: 3,
+      installmentValue: 400,
+      discount: 70,
+      total: 1200,
+    },
+    {
+      installments: 6,
+      installmentValue: 250,
+      discount: 50,
+      total: 1500,
+    },
+  ],
+};
 export const getOffersResponseMock: GetOffers.Model[] = [
-  {
-    value: {
-      actual: 123.0,
-      original: 123.0,
-    },
-    date: new Date(),
-    contract: '12345677890',
-    description: '',
-    installmentPlans: [
-      {
-        installments: 10,
-        installmentValue: 123,
-        discount: 90,
-        total: 99,
-      },
-    ],
-    type: DebtType.creditCard,
-  },
-  {
-    value: {
-      actual: 123.0,
-      original: 123.0,
-    },
-    date: new Date(),
-    contract: '12345677890',
-    description: '',
-    installmentPlans: [
-      {
-        installments: 10,
-        installmentValue: 123,
-        discount: 90,
-        total: 99,
-      },
-    ],
-    type: DebtType.creditCard,
-  },
-  {
-    value: {
-      actual: 123.0,
-      original: 123.0,
-    },
-    date: new Date(),
-    contract: '12345677890',
-    description: '',
-    installmentPlans: [
-      {
-        installments: 10,
-        installmentValue: 123,
-        discount: 90,
-        total: 99,
-      },
-    ],
-    type: DebtType.creditCard,
-  },
+  item,
+  { ...item, id: faker.datatype.uuid() },
 ];
