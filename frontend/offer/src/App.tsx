9d289c77b950340efdf9e9f73a68865f89ba266f
Patrick Coutinho <patrick@istyle.com.br>
2022-07-28 14:01:47 -0300

    chore: dockerizing adjustments

--- frontend/offer/src/App.tsx
+++ frontend/offer/src/App.tsx
@@ -3,7 +3,7 @@ import RemoteNavbar from 'design_system/Navbar';
 import RemoteFooter from 'design_system/Footer';
 import GetOffersPage from './presentation/get-offers/get-offers';
 import { RemoteGetOffers } from './data/get-offers/remote-get-offers';
-import { FetchHttpClient } from '@shared/infra';
+import { FetchHttpClient } from 'shared_infra/infra';
 
 const App: FC = () => {
   return (
